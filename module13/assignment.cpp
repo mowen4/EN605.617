e7df2c6457ace8cee544e69dc92ea6e3d56bf80b
mowen4 <98691260+mowen4@users.noreply.github.com>
2022-04-27 16:53:22 -0400

    Update assignment.cpp

--- module13/assignment.cpp
+++ module13/assignment.cpp
@@ -317,7 +317,7 @@ int main(int argc, char** argv)
 	cl_event event1;
     size_t gWI = NUM_BUFFER_ELEMENTS;
 
-	for (int i = 0 ; i < 2; i++)
+	for (int i = 0 ; i < 5; i++)
 	{
 
 		errNum = clEnqueueNDRangeKernel(
@@ -331,8 +331,8 @@ int main(int argc, char** argv)
 		  0, 
 		  &event0);
 		
-
-		errNum = clEnqueueMarker(queue1, &event1);
+		//shouldnt be needed
+		//errNum = clEnqueueMarker(queue1, &event1);
 
 		errNum = clEnqueueNDRangeKernel(
 		  queue1, 
